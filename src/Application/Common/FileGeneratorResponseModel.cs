3fef2c3e30f3ebeedae6b8711d5f5de5dc34bb55
ABDULSALAM Ahmad Ayoola <33053528+treehays@users.noreply.github.com>
2024-11-12 11:44:27 +0100

    Delete FileGeneratorResponseModel.cs

--- src/Application/Common/FileGeneratorResponseModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TajneedApi.Application.Common;
-
-public class FileResponseModel
-{
-    public byte[]? FileContent { get; set; }
-    public string? FileName { get; set; }
-    public string? ContentType { get; set; }
-}
